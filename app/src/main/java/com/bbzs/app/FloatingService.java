c74b4ed0ef841ee36727e4ad3cebfe35e8c18db5
panshi6688 <1120760396@qq.com>
2026-02-27 16:41:57 +0800

    日志记录1.2

--- app/src/main/java/com/bbzs/app/FloatingService.java
+++ app/src/main/java/com/bbzs/app/FloatingService.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.net.Uri;
